64b9c0142e49181802152a3cb481e97bfdc89ea4
JaneV <13032660329@163.com>
2020-08-03 10:35:08 +0800

    修复激活问题

--- app/src/main/java/com/yunbiao/ybsmartcheckin_live_id/SplashActivity.java
+++ app/src/main/java/com/yunbiao/ybsmartcheckin_live_id/SplashActivity.java
@@ -244,11 +244,10 @@ public class SplashActivity extends BaseActivity {
             if(Constants.FLAVOR_TYPE == FlavorType.XENON){
                 FaceSDKActive.activeLocal(FaceSDKActive.YB_APPID,FaceSDKActive.YB_SDKKEY,activeCallback);
             } else {
-                FaceSDKActive.active(
-                        Constants.DEVICE_TYPE == Constants.DeviceType.TEMPER_SAFETY_CHECK
-                        || Constants.DEVICE_TYPE == Constants.DeviceType.HT_TEMPER_SAFETY_CHECK
-                        || Constants.DEVICE_TYPE == Constants.DeviceType.TEMPERATURE_MEASUREMENT_5_INCH
-                        ,activeCallback);
+                boolean canGo = Constants.DEVICE_TYPE != Constants.DeviceType.TEMPER_SAFETY_CHECK
+                        && Constants.DEVICE_TYPE != Constants.DeviceType.HT_TEMPER_SAFETY_CHECK
+                        && Constants.DEVICE_TYPE != Constants.DeviceType.TEMPERATURE_MEASUREMENT_5_INCH;
+                FaceSDKActive.active(canGo,activeCallback);
             }
         }).start();
     };
